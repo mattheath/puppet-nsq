8a5ca98792c991d55c4a18a50354c5d7b590a755
Matt Heath <matt@mattheath.com>
2013-03-04 01:04:25 +0000

    Add bin dir to config

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,8 +1,9 @@
 class nsq::config {
   require boxen::config
 
-  $configdir  = "${boxen::config::configdir}/nsq"
-  $datadir    = "${boxen::config::datadir}/nsq"
-  $logdir     = "${boxen::config::logdir}/nsq"
+  $configdir = "${boxen::config::configdir}/nsq"
+  $datadir   = "${boxen::config::datadir}/nsq"
+  $logdir    = "${boxen::config::logdir}/nsq"
+  $bindir    = "${boxen::config::homebrewdir}/bin"
 
 }
