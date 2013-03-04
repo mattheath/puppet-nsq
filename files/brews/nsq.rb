e7d04ec9d1ffb053f77bd51676ca1befffaa83a1
Matt Heath <matt@mattheath.com>
2013-03-04 00:35:14 +0000

    Create brew to install nsq

---
+++ files/brews/nsq.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Nsq < Formula
+  homepage 'https://github.com/bitly/nsq'
+  url 'https://s3.amazonaws.com/bitly-downloads/nsq/nsq-0.2.18.darwin-amd64.tar.gz'
+  sha1 '63333c5d54a66bda03b2327b15d8a98eb9989b9d'
+
+  version '0.2.18-boxen1'
+
+  def install
+    # We only need to symlink files in ./bin into the path
+    prefix.install Dir['bin']
+  end
+end
