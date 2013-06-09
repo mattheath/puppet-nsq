eaf57f68e7ec9395dd4eb1b803220b46ea81dc9b
Matt Heath <matt@mattheath.com>
2013-06-09 10:02:54 +0100

    Correct SHA signature to 0.2.21

--- files/brews/nsq.rb
+++ files/brews/nsq.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Nsq < Formula
   homepage 'https://github.com/bitly/nsq'
   url 'https://s3.amazonaws.com/bitly-downloads/nsq/nsq-0.2.21.darwin-amd64.go1.0.3.tar.gz'
-  sha1 '63333c5d54a66bda03b2327b15d8a98eb9989b9d'
+  sha1 'b1e31a5684493954468b7204d1f4e31181964da1'
 
   version '0.2.21-boxen1'
 
