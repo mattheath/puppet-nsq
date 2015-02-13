035c14a0857c9ab31940af208398dbc163448907
Matt Heath <matt@mattheath.com>
2015-02-13 00:18:10 +0000

    Bump to 0.3.2

--- files/brews/nsq.rb
+++ files/brews/nsq.rb
@@ -2,10 +2,10 @@ require 'formula'
 
 class Nsq < Formula
   homepage 'https://github.com/bitly/nsq'
-  url 'https://s3.amazonaws.com/bitly-downloads/nsq/nsq-0.2.21.darwin-amd64.go1.0.3.tar.gz'
-  sha1 'b1e31a5684493954468b7204d1f4e31181964da1'
+  url 'https://s3.amazonaws.com/bitly-downloads/nsq/nsq-0.3.2.darwin-amd64.go1.4.1.tar.gz'
+  sha1 '0c1866a01b879af4730e09e02e600cbe2930511c'
 
-  version '0.2.21-boxen1'
+  version '0.3.2-boxen1'
 
   def install
     # Symlink files in ./bin into the path
